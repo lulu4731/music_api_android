9a2052f871aa66cdc622387d770c56f8e4c5b309
Vinh <vinhvipvl@gmail.com>
2022-04-30 23:49:46 +0700

    file upload limit size

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 const express = require('express');
 const app = express();
-const bodyParser = require('body-parser')
 const dotenv = require('dotenv');
 const db = require('./src/database');
 const swaggerUi = require('swagger-ui-express');
@@ -12,23 +11,19 @@ const fileUpload = require('express-fileupload');
 
 dotenv.config();
 db.connect()
-app.use(express.json());
+
 app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
-app.use(fileUpload());
+app.use(express.json({limit: '30mb'}));
+app.use(express.urlencoded({extended: false, limit: '30mb'}));
 
-// for parsing application/x-www-form-urlencoded
-app.use(express.urlencoded({ extended: true }));
+app.use(fileUpload({
+    limits: {
+        fileSize: 30*1024*1024
+    },
+    abortOnLimit: true
+}));
 
-// for parsing multipart/form-data
-// app.use(upload.array());
-app.use(express.static('public'));
-
-
-app.use(bodyParser.urlencoded({ extended: false }));
-
-// parse application/json
-app.use(bodyParser.json());
 
 app.use((req, res, next) => {
     res.header('Access-Control-Allow-Origin', '*');
